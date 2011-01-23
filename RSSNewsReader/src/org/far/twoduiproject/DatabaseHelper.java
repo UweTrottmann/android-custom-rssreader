28f1c9a220d737972c9c385cfab7a085f5d73ae7
trip.mckay@gmail.com <trip.mckay@gmail.com@caeef64a-e14a-2368-7cc0-55d4de1a3ffe>
2011-01-23 12:17:23 +0000

    Implemented database schema, constructors.

--- RSSNewsReader/src/org/far/twoduiproject/DatabaseHelper.java
+++ RSSNewsReader/src/org/far/twoduiproject/DatabaseHelper.java
@@ -1,36 +1,98 @@
 package org.far.twoduiproject;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
-public class DatabaseHelper extends SQLiteOpenHelper {
-
-	private static DatabaseHelper _instance;
+public class DatabaseHelper {
 	private static final String DATABASE_NAME = "rssdb";
 	private static final int DATABASE_VERSION = 1;
-	
+	public static final String ITEM_TABLE = "items";
+	public static final String ITEM_ID = "_id";
+	public static final String TITLE = "title";
+	public static final String LINK = "link";
+	public static final String DESCRIPTION = "description";
+	public static final String PUBDATE = "pubdate";
+	public static final String CATEGORY_TABLE = "categories";
+	public static final String ITEM_CATEGORY = "itemcategory";
+	public static final String CATEGORY_ID = "_id";
+	public static final String CATEGORY_NAME = "name";
+	public static final String TAG = "SeriesGuide.DatabaseHelper";
+
+	private static DatabaseHelper _instance;
+	private Context context;
+	private SQLiteDatabase db;
+
 	private DatabaseHelper(Context context) {
-		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		this.context = context;
+		this.db = new OpenHelper(context).getWritableDatabase();
 	}
-	
-	public DatabaseHelper getInstance(Context context){
+
+	public DatabaseHelper getInstance(Context context) {
 		if (_instance == null) {
 			_instance = new DatabaseHelper(context);
 		}
 		return _instance;
 	}
 	
-	@Override
-	public void onCreate(SQLiteDatabase arg0) {
-		// TODO Auto-generated method stub
-
+	public Cursor getItemsForCategory(int category_id){
+		// TODO: Implement fetching of items within category
+		return null;
 	}
 
-	@Override
-	public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
-		// TODO Auto-generated method stub
+	private class OpenHelper extends SQLiteOpenHelper {
+
+		private OpenHelper(Context context) {
+			super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		}
+
+		@Override
+		public void onCreate(SQLiteDatabase arg0) {
+			/*
+			 * Be aware that the current version of sqlite on Android does NOT
+			 * support foreign keys or constraints. It is not that big of a
+			 * downside, as it is more a convenience to have those (e.g. cascade
+			 * on delete).
+			 */
+			db.execSQL("create table " + ITEM_TABLE + " ("
+					+ ITEM_ID + " int primary key,"
+					+ TITLE + " text,"
+					+ LINK + " text default '',"
+					+ DESCRIPTION + " text default '',"
+					+ PUBDATE + " text default '',"
+					+ ITEM_CATEGORY + " int references " + CATEGORY_TABLE + "(" + CATEGORY_ID + ")"
+					+ ");");
+			db.execSQL("create table " + CATEGORY_TABLE + " ("
+					+ CATEGORY_ID + " int primary key,"
+					+ CATEGORY_NAME + " text default '',"
+					+ ");");
+		}
+
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+			Log.w(TAG, "Upgrading database from version " + oldVersion + " to " + newVersion);
+			if (oldVersion > DATABASE_VERSION) {
+				Log.w(TAG, "Database is newer version (" + oldVersion + ") than this app can use (" + newVersion + "), starting from scratch");
+				db.execSQL("DROP TABLE IF EXISTS " + ITEM_TABLE);
+				db.execSQL("DROP TABLE IF EXISTS " + CATEGORY_TABLE);
+				db.setVersion(DATABASE_VERSION);
+				onCreate(db);
+				return;
+			}
+			
+			/*
+			 * Upgrade-Code if the need arises to change the db schema between versions
+			 */
+//			int currentVersion = oldVersion;
+//			while (currentVersion < newVersion) {
+//				switch (currentVersion) {
+//				}
+//				currentVersion++;
+//			}
+//			db.setVersion(newVersion);
+		}
 
 	}
-
 }
