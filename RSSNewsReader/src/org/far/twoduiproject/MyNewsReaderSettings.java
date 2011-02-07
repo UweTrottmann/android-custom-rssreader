dd1c20050da15e534c0737e23a99b8cf539cf862
trip.mckay@gmail.com <trip.mckay@gmail.com@caeef64a-e14a-2368-7cc0-55d4de1a3ffe>
2011-02-07 19:26:11 +0000

    Fixed preferences :D

--- RSSNewsReader/src/org/far/twoduiproject/MyNewsReaderSettings.java
+++ RSSNewsReader/src/org/far/twoduiproject/MyNewsReaderSettings.java
@@ -1,14 +1,10 @@
+
 package org.far.twoduiproject;
-
-
-import org.far.twoduiproject.BbcActivity;
-import org.far.twoduiproject.CnnActivity;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,54 +12,45 @@ import android.widget.ImageView;
 
 public class MyNewsReaderSettings extends Activity {
     /** Called when the activity is first created. */
-	
-	public static final String PROVIDER_PREF="providerpref";
-	public static final String NAME_PROVIDER_PREF = "providerprefname";
-	SharedPreferences provider;
-	
+
+    public static final String PROVIDER_PREF = "providerpref";
+
+    public static final String NAME_PROVIDER_PREF = "providerprefname";
+
+    SharedPreferences provider;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
-        
-        provider = getSharedPreferences(PROVIDER_PREF,Context.MODE_PRIVATE);
-        ImageView cnnlogo = (ImageView)findViewById(R.id.cnn);
-        ImageView bbclogo = (ImageView)findViewById(R.id.bbc);
-        
-        
- 
-        cnnlogo.setOnClickListener( new OnClickListener(){
 
-			@Override
-			public void onClick(View v) {
-				
-				Editor editor = provider.edit();
-				editor.putString(NAME_PROVIDER_PREF,"cnn");
-				editor.commit();
-				
-				startActivity(new Intent(MyNewsReaderSettings.this, PreferenceActivity.class ));
-			
-			}
-        
+        provider = getSharedPreferences(PROVIDER_PREF, Context.MODE_PRIVATE);
+        ImageView cnnlogo = (ImageView) findViewById(R.id.cnn);
+        ImageView bbclogo = (ImageView) findViewById(R.id.bbc);
+
+        cnnlogo.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MyNewsReaderSettings.this, PreferenceActivity.class);
+                i.putExtra(DatabaseHelper.PROVIDER_ID, 1);
+                startActivity(i);
+
+            }
+
         });
-        
+
         bbclogo.setOnClickListener(new OnClickListener() {
 
-			@Override
-			public void onClick(View v) {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MyNewsReaderSettings.this, PreferenceActivity.class);
+                i.putExtra(DatabaseHelper.PROVIDER_ID, 0);
+                startActivity(i);
 
-				Editor editor = provider.edit();
-				editor.putString(NAME_PROVIDER_PREF, "bbc");
-				editor.commit();
-				startActivity(new Intent(MyNewsReaderSettings.this,PreferenceActivity.class));
-				
-			}
-        	
-        	
-        	
+            }
+
         });
-        
-        
-        
+
     }
 }
--- RSSNewsReader/src/org/far/twoduiproject/PreferenceActivity.java
+++ RSSNewsReader/src/org/far/twoduiproject/PreferenceActivity.java
@@ -1,12 +1,9 @@
-package org.far.twoduiproject;
 
+package org.far.twoduiproject;
 
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.app.ListActivity;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.SparseBooleanArray;
@@ -20,135 +17,125 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class PreferenceActivity extends Activity {
     /** Called when the activity is first created. */
-	
-	public static final String PROVIDER_PREF="providerpref";
-	public static final String NAME_PROVIDER_PREF = "providerprefname";
 
-	
-	private DatabaseHelper mDbHelper = DatabaseHelper.getInstance(this);
-	
+    public static final String PROVIDER_PREF = "providerpref";
+
+    public static final String NAME_PROVIDER_PREF = "providerprefname";
+
+    private DatabaseHelper mDbHelper = DatabaseHelper.getInstance(this);
 
     private ArrayList<String> categoryNameList = new ArrayList<String>();
+
     private ArrayList<Category> categoryList = new ArrayList<Category>();
-	
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preference);
-        int providerid=0;
-        
-        SharedPreferences provider = getSharedPreferences(PROVIDER_PREF,Context.MODE_PRIVATE);
+        int providerid = 0;
 
-        
-        //For CNN, provider id = 1, For BBC, provider id = 2 - if preference doesn't exist - always pick cnn as default
-        if (provider.getString(NAME_PROVIDER_PREF,"cnn")=="cnn")
-        	providerid=1;
-        else
-        	providerid=2;
-        
-        
+        // For CNN, provider id = 1, For BBC, provider id = 0 - if preference
+        // doesn't exist - always pick cnn as default
+        providerid = getIntent().getExtras().getInt(DatabaseHelper.PROVIDER_ID);
+
         mDbHelper = DatabaseHelper.getInstance(getApplicationContext());
- 
-        
-        String getCategoryQuery = "select preferences.pref_categoryid,categories.name,preferences.enabled from " + DatabaseHelper.PREFERENCE_TABLE + " inner join " + DatabaseHelper.CATEGORY_TABLE + " on "+DatabaseHelper.PREFERENCE_TABLE+"."+DatabaseHelper.PREF_CATEGORY_ID + "="+DatabaseHelper.CATEGORY_TABLE+"."+DatabaseHelper.CATEGORY_ID+" where preferences.Enabled = 1 and "+DatabaseHelper.PREF_PROVIDERID+" = " + providerid + ";";
-        
-        //String getCategoryQuery = "select pref_categoryid,"
-        
-       //String getCategoryQuery ="select pref_categoryid,feedpath,enabled from preferences;";
-        
-        
+
+        String getCategoryQuery = "select preferences.pref_categoryid,categories.name,preferences.enabled from "
+                + DatabaseHelper.PREFERENCE_TABLE
+                + " inner join "
+                + DatabaseHelper.CATEGORY_TABLE
+                + " on "
+                + DatabaseHelper.PREFERENCE_TABLE
+                + "."
+                + DatabaseHelper.PREF_CATEGORY_ID
+                + "="
+                + DatabaseHelper.CATEGORY_TABLE
+                + "."
+                + DatabaseHelper.CATEGORY_ID
+                + " where " + DatabaseHelper.PREF_PROVIDERID + " = " + providerid + ";";
+
         Cursor preferenceCategory = mDbHelper.getCategories(getCategoryQuery);
-        
-        while(preferenceCategory.moveToNext()){
-        	categoryNameList.add(preferenceCategory.getString(1));
-        	categoryList.add(new Category(preferenceCategory.getInt(0),preferenceCategory.getString(1),preferenceCategory.getInt(2)));
+
+        while (preferenceCategory.moveToNext()) {
+            categoryNameList.add(preferenceCategory.getString(1));
+            categoryList.add(new Category(preferenceCategory.getInt(0), preferenceCategory
+                    .getString(1), preferenceCategory.getInt(2)));
         }
         
+        preferenceCategory.close();
 
-        
-        final ListView preferenceListView = (ListView)findViewById(R.id.prefList);
-        preferenceListView.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_multiple_choice,categoryNameList));
+        final ListView preferenceListView = (ListView) findViewById(R.id.prefList);
+        preferenceListView.setAdapter(new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_multiple_choice, categoryNameList));
         preferenceListView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
         preferenceListView.setOnItemClickListener(preferenceListViewListener);
-        
-       
-        
-                
-        Button saveButton = (Button)findViewById(R.id.save);
+
+        Button saveButton = (Button) findViewById(R.id.save);
         saveButton.setEnabled(false);
         saveButton.setTag(preferenceListView);
         saveButton.setOnClickListener(saveButtonListener);
-        
+
     }
-    
-    private OnItemClickListener preferenceListViewListener = new OnItemClickListener(){
 
-		@Override
-		public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
-				long arg3) {
+    private OnItemClickListener preferenceListViewListener = new OnItemClickListener() {
 
-			Button saveButton = (Button)findViewById(R.id.save);
-			saveButton.setEnabled(true);
-			
-		}
-    	
+        @Override
+        public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+
+            Button saveButton = (Button) findViewById(R.id.save);
+            saveButton.setEnabled(true);
+
+        }
+
     };
-    
 
-	private OnClickListener saveButtonListener= new OnClickListener(){
-   	
-		@Override
-		public void onClick(View v) {
-			
-			String updateQueryEnabled = "";
-			String updateQueryDisabled ="";
-			String enabledIds = "";
-			String disabledIds = "" ;
-			ListView preferenceListView = (ListView)v.getTag();
-			
-			
-			//call update method to update the isEnabled flag of the table for each category
-			
-			SparseBooleanArray checked = preferenceListView.getCheckedItemPositions();
+    private OnClickListener saveButtonListener = new OnClickListener() {
 
-			for (int i =0; i<categoryList.size();i++){
-				if (checked.get(i)){
-					categoryList.get(i).setEnabled(1);
-					enabledIds = enabledIds + String.valueOf(categoryList.get(i).getCategoryId());
-					enabledIds = enabledIds + ",";
-					
-				}
-				else{
-					categoryList.get(i).setEnabled(0);
-					
-					disabledIds = disabledIds + String.valueOf(categoryList.get(i).getCategoryId());
-					disabledIds = disabledIds + ",";
-					
-				}
-			}
-			
+        @Override
+        public void onClick(View v) {
 
-			
-			//enabledIds = enabledIds.substring(0, enabledIds.length()-1);
-			//disabledIds = disabledIds.substring(0,disabledIds.length()-1);
-			
-			enabledIds = enabledIds.substring(0, enabledIds.length()-1);
-			disabledIds = disabledIds.substring(0, disabledIds.length()-1);
-			
-			
-			updateQueryEnabled = "update " + DatabaseHelper.PREFERENCE_TABLE + " set enabled = 1 where pref_categoryid in (" + enabledIds + ");" ;
-			updateQueryDisabled = "update " + DatabaseHelper.PREFERENCE_TABLE + " set enabled = 0 where pref_categoryid in (" + disabledIds + ");";
-			
-			mDbHelper.changeCategoryState(updateQueryEnabled, updateQueryDisabled);
+            String updateQueryEnabled = "";
+            String updateQueryDisabled = "";
+            String enabledIds = "";
+            String disabledIds = "";
+            ListView preferenceListView = (ListView) v.getTag();
 
-			
-		}
+            // call update method to update the isEnabled flag of the table for
+            // each category
 
+            SparseBooleanArray checked = preferenceListView.getCheckedItemPositions();
 
+            for (int i = 0; i < categoryList.size(); i++) {
+                if (checked.get(i)) {
+                    categoryList.get(i).setEnabled(1);
+                    enabledIds = enabledIds + String.valueOf(categoryList.get(i).getCategoryId());
+                    enabledIds = enabledIds + ",";
 
-    	
-    	
+                } else {
+                    categoryList.get(i).setEnabled(0);
+
+                    disabledIds = disabledIds + String.valueOf(categoryList.get(i).getCategoryId());
+                    disabledIds = disabledIds + ",";
+
+                }
+            }
+
+            if (enabledIds.length() != 0) {
+                enabledIds = enabledIds.substring(0, enabledIds.length() - 1);
+            }
+            if (disabledIds.length() != 0) {
+                disabledIds = disabledIds.substring(0, disabledIds.length() - 1);
+            }                   
+
+            updateQueryEnabled = "update " + DatabaseHelper.PREFERENCE_TABLE
+                    + " set enabled = 1 where pref_categoryid in (" + enabledIds + ");";
+            updateQueryDisabled = "update " + DatabaseHelper.PREFERENCE_TABLE
+                    + " set enabled = 0 where pref_categoryid in (" + disabledIds + ");";
+
+            mDbHelper.changeCategoryState(updateQueryEnabled, updateQueryDisabled);
+
+        }
+
     };
-    
-    
+
 }
